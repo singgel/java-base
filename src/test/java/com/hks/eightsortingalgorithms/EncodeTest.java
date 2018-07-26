bcb396b4d07d1660a00e7bc1474375460467d4ac
heks <heks@yiche.com>
2018-07-26 11:11:37 +0800

    String的encode编码

--- src/test/java/com/hks/eightsortingalgorithms/EncodeTest.java
+++ src/test/java/com/hks/eightsortingalgorithms/EncodeTest.java
@@ -1,5 +1,10 @@
 package com.hks.eightsortingalgorithms;
 
+import java.io.UnsupportedEncodingException;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+
 public class EncodeTest {
 
     public static void main(String[] ags){
@@ -8,6 +13,20 @@ public class EncodeTest {
             byte[] b = s.getBytes("UTF-8");
             String n = new String(b,"UTF-8");
             System.out.print(n);
+
+            /*ByteToCharConverter charConverter = ByteToCharConverter.getConverter("UTF-8");
+            char c[] = charConverter.convertAll(byteArray);
+            CharToByteConverter byteConverter = CharToByteConverter.getConverter("UTF-8");
+            byte[] b = byteConverter.convertAll(c);*/
+
+            Charset charset = Charset.forName("UTF-8");
+            ByteBuffer byteBuffer = charset.encode(n);
+            CharBuffer charBuffer = charset.decode(byteBuffer);
+            System.out.print(charBuffer);
+
+            ByteBuffer heapByteBuffer = ByteBuffer.allocate(1024);
+            ByteBuffer byteBuffer1 = heapByteBuffer.putChar(n.charAt(0));
+            System.out.print(byteBuffer1);
         }catch (Exception ex){
             ex.printStackTrace();
         }
