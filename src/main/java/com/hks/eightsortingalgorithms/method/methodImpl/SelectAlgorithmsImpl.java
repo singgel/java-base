2c3f311d5a17cf73cd98f60fd9e6933a9d1d03e4
heks <heks@yiche.com>
2018-07-31 15:27:13 +0800

    选择排序

--- src/main/java/com/hks/eightsortingalgorithms/method/methodImpl/SelectAlgorithmsImpl.java
+++ src/main/java/com/hks/eightsortingalgorithms/method/methodImpl/SelectAlgorithmsImpl.java
@@ -1,6 +1,31 @@
 package com.hks.eightsortingalgorithms.method.methodImpl;
 
 import com.hks.eightsortingalgorithms.method.SelectAlgorithms;
+import org.springframework.stereotype.Component;
 
+@Component
 public class SelectAlgorithmsImpl implements SelectAlgorithms {
+
+    private static void exchang(int[] s, int i, int j) {
+        int temp = s[j];
+        s[j] = s[i];
+        s[i] = temp;
+    }
+
+    @Override
+    public int[] sort(int[] intArr) {
+        int temp = 0;
+        for (int i = 0; i < intArr.length - 1; i++) {
+            temp = i;
+            for (int j = i + 1; j < intArr.length; j++) {
+                if (intArr[temp] > intArr[j]) {
+                    temp = j;
+                }
+            }
+            if (temp != i) {
+                exchang(intArr, i, temp);
+            }
+        }
+        return intArr;
+    }
 }
