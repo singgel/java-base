de14c7e22039322025780deca2de392182601f8d
heks <heks@yiche.com>
2018-08-16 09:20:57 +0800

    添加一个时间计时aop

--- src/main/java/com/hks/eightsortingalgorithms/aspect/MethodTimeAspect.java
+++ src/main/java/com/hks/eightsortingalgorithms/aspect/MethodTimeAspect.java
@@ -1,17 +1,18 @@
 package com.hks.eightsortingalgorithms.aspect;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
 public class MethodTimeAspect {
 
-    private static final Log LOG = LogFactory.getLog(MethodTimeAspect.class);
+    private static final Logger logger = LoggerFactory.getLogger(MethodTimeAspect.class);
 
     /**
      * 定义一个切入点.
@@ -24,19 +25,19 @@ public class MethodTimeAspect {
      */
     @Pointcut("execution(* com.hks.eightsortingalgorithms.method..*.*(..))")
     public void logPointcut(){}
-    @org.aspectj.lang.annotation.Around("logPointcut()")
+    @Around("logPointcut()")
 
     public Object doAround(ProceedingJoinPoint joinPoint) throws Throwable{
         long start = System.currentTimeMillis();
         try {
             Object result = joinPoint.proceed();
             long end = System.currentTimeMillis();
-            LOG.error("+++++around " + joinPoint + "\tUse time : " + (end - start) + " ms!");
+            logger.error("+++++around " + joinPoint + "\tUse time : " + (end - start) + " ms!");
             return result;
 
         } catch (Throwable e) {
             long end = System.currentTimeMillis();
-            LOG.error("+++++around " + joinPoint + "\tUse time : " + (end - start) + " ms with exception : " + e.getMessage());
+            logger.error("+++++around " + joinPoint + "\tUse time : " + (end - start) + " ms with exception : " + e.getMessage());
             throw e;
         }
 
