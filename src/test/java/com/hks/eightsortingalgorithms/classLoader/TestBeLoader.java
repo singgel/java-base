b04f8b3e320d91346aa43759d047fd50e8465fdc
heks <heks@yiche.com>
2018-08-20 16:51:30 +0800

    JVM自定义classLoader来实现类的加载，验证动态扩展一个类装载器

---
+++ src/test/java/com/hks/eightsortingalgorithms/classLoader/TestBeLoader.java
@@ -0,0 +1,15 @@
+package com.hks.eightsortingalgorithms.classLoader;
+
+/**
+ * 第二步，定义一个类，专门用于被装载，这里我们定义了一个静态代码块，待会用到它
+ */
+public class TestBeLoader {
+
+    static{
+        System.out.println("TestBeLoader init");
+    }
+    public void sayHello(){
+        System.out.println("hello");
+    }
+
+}
---
+++ src/test/java/com/hks/eightsortingalgorithms/classLoader/TestClassLoaderDemo.java
@@ -0,0 +1,27 @@
+package com.hks.eightsortingalgorithms.classLoader;
+
+/**
+ * 第三步，定义一个有main函数入口的public类来做验证
+ */
+public class TestClassLoaderDemo {
+
+    public static void main(String[] args) throws InstantiationException, IllegalAccessException {
+        Class thisCls = TestClassLoaderDemo.class;
+        MyClassLoader myClassLoader = new MyClassLoader();
+        System.out.println(thisCls.getClassLoader());
+        System.out.println(myClassLoader.getParent());
+        try {
+            //用自定义的类装载器来装载类,这是动态扩展的一种途径
+            Class cls2 = myClassLoader.loadClass("com.hks.eightsortingalgorithms.classLoader.TestBeLoader");
+            System.out.println(cls2.getClassLoader());
+            TestBeLoader test=(TestBeLoader)cls2.newInstance();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 第四步，查看运行结果
+     */
+
+}
