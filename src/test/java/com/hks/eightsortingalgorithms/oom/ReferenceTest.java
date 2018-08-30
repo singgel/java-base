8ffb0764693466f53d66691a337f9263df5371be
heks <heks@yiche.com>
2018-08-30 17:34:45 +0800

    object对象的引用

---
+++ src/test/java/com/hks/eightsortingalgorithms/oom/ReferenceTest.java
@@ -0,0 +1,19 @@
+package com.hks.eightsortingalgorithms.oom;
+
+public class ReferenceTest {
+
+    public static void main(String[] args){
+        String a = "1";
+        String b = "2";
+        String c = "3";
+        a=b;
+        System.out.println(a+"\n");
+        a=c;
+        System.out.println(a+"\n");
+        c=null;
+        System.out.println(a+"\n");
+        a=null;
+        System.out.println(a+"\n");
+    }
+
+}
