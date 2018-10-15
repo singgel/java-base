2f0eb31234b0c97f2f3c7933dc0dbaf784c49269
heks <heks@yiche.com>
2018-10-15 10:06:04 +0800

    1.Serialize序列化与反序列化 2.序列化

---
+++ src/test/java/com/hks/eightsortingalgorithms/serializable/DeserializeDemo.java
@@ -0,0 +1,37 @@
+package com.hks.eightsortingalgorithms.serializable;
+
+import java.io.*;
+/**
+ * @Author: hekuangsheng
+ * @Date: 2018/10/15
+ */
+
+public class DeserializeDemo
+{
+    public static void main(String [] args)
+    {
+        Employee e = null;
+        try
+        {
+            FileInputStream fileIn = new FileInputStream("/Users/yiche/tmp/employee.ser");
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            e = (Employee) in.readObject();
+            in.close();
+            fileIn.close();
+        }catch(IOException i)
+        {
+            i.printStackTrace();
+            return;
+        }catch(ClassNotFoundException c)
+        {
+            System.out.println("Employee class not found");
+            c.printStackTrace();
+            return;
+        }
+        System.out.println("Deserialized Employee...");
+        System.out.println("Name: " + e.name);
+        System.out.println("Address: " + e.address);
+        System.out.println("SSN: " + e.SSN);
+        System.out.println("Number: " + e.number);
+    }
+}
