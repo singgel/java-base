fa851ffb7615dfc5ecfebfdeaf3c6514b9de8e49
heks <heks@yiche.com>
2018-10-25 20:08:11 +0800

    nowcoder 输入某二叉树的前序遍历和中序遍历的结果，请重建出该二叉树。假设输入的前序遍历和中序遍历的结果中都不含重复的数字。例如输入前序遍历序列{1,2,4,7,3,5,6,8}和中序遍历序列{4,7,2,1,5,3,8,6}，则重建二叉树并返回。

---
+++ src/main/java/com/hks/eightsortingalgorithms/nowcoder/RebuildBinaryTree.java
@@ -0,0 +1,60 @@
+package com.hks.eightsortingalgorithms.nowcoder;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @Author: hekuangsheng
+ * @Date: 2018/10/25
+ * <p>
+ * <p>
+ * <p>
+ * 递归思想，每次将左右两颗子树当成新的子树进行处理，中序的左右子树索引很好找，
+ * <p>
+ * 前序的开始结束索引通过计算中序中左右子树的大小来计算，然后递归求解，
+ * <p>
+ * 直到startPre>endPre||startIn>endIn说明子树整理完到。
+ * <p>
+ * 方法每次返回左子树活右子树的根节点
+ */
+public class RebuildBinaryTree {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode reConstructBinaryTree(int[] pre, int[] in) {
+        TreeNode root = reConstructBinaryTree(pre, 0, pre.length - 1, in, 0, in.length - 1);
+        return root;
+    }
+
+    private TreeNode reConstructBinaryTree(int[] pre, int startPre, int endPre, int[] in, int startIn, int endIn) {
+
+        if (startPre > endPre || startIn > endIn) {
+            return null;
+        }
+        TreeNode root = new TreeNode(pre[startPre]);
+
+        for (int i = startIn; i <= endIn; i++) {
+            if (in[i] == pre[startPre]) {
+                root.left = reConstructBinaryTree(pre, startPre + 1, startPre + i - startIn, in, startIn, i - 1);
+                root.right = reConstructBinaryTree(pre, i - startIn + startPre + 1, endPre, in, i + 1, endIn);
+                break;
+            }
+        }
+        return root;
+    }
+
+    @Test
+    public void test() {
+        int[] pre = {1, 2, 4, 7, 3, 5, 6, 8};
+        int[] in = {4, 7, 2, 1, 5, 3, 8, 6};
+        TreeNode treeNode = reConstructBinaryTree(pre, in);
+    }
+
+}
