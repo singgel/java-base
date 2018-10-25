fa3d88374643a43a2d12b98f1a3caeeec6fd7f89
heks <heks@yiche.com>
2018-10-25 16:34:14 +0800

    1.LeetCode 2.单元测试

---
+++ src/main/java/com/hks/eightsortingalgorithms/leetcode/TwoSum.java
@@ -0,0 +1,44 @@
+package com.hks.eightsortingalgorithms.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @Author: hekuangsheng
+ * @Date: 2018/10/25
+ * <p>
+ * 示例:
+ * <p>
+ * 给定 nums = [2, 7, 11, 15], target = 9
+ * <p>
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]
+ */
+public class TwoSum {
+
+    int[] nums = {2, 7, 11, 15};
+    int target = 9;
+    int[] except = {0, 1};
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("不存在这两个数");
+    }
+
+    @Test
+    public void test() {
+        assertArrayEquals(except, twoSum(nums, target));
+    }
+}
