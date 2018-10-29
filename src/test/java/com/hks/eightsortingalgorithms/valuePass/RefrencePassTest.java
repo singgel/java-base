ec1b8b6f7580b6c8f8faf30574bdda37ee00d80e
heks <heks@yiche.com>
2018-10-29 10:33:30 +0800

    因此可见：在Java中所有的参数传递，不管基本类型还是引用类型，都是值传递，或者说是副本传递。 只是在传递过程中：
    如果是对基本数据类型的数据进行操作，由于原始内容和副本都是存储实际值，并且是在不同的栈区，因此形参的操作，不影响原始内容。
    
    如果是对引用类型的数据进行操作，分两种情况，一种是形参和实参保持指向同一个对象地址，则形参的操作，会影响实参指向的对象的内容。一种是形参被改动指向新的对象地址（如重新赋值引用），则形参的操作，不会影响实参指向的对象的内容。

---
+++ src/test/java/com/hks/eightsortingalgorithms/valuePass/RefrencePassTest.java
@@ -0,0 +1,32 @@
+package com.hks.eightsortingalgorithms.valuePass;
+
+/**
+ * @Author: hekuangsheng
+ * @Date: 2018/10/29
+ * <p>
+ * 引用传递：
+ * ”引用”也就是指向真实内容的地址值，在方法调用时，
+ * 实参的地址通过方法调用被传递给相应的形参，在方法体内，
+ * 形参和实参指向相同的内存地址，对形参的操作会影响的真实内容。
+ */
+public class RefrencePassTest {
+
+    public static void PersonCrossTest(Person person) {
+        System.out.println("传入的person的name：" + person.getName());
+        person.setName("我是张小龙");
+
+        //加多此行代码
+        ///无论是基本类型和是引用类型，在实参传入形参时，都是值传递，也就是说传递的都是一个副本，而不是内容本身。
+        person = new Person();
+        person.setName("啊哈");
+        System.out.println("方法内重新赋值后的name：" + person.getName());
+    }
+
+    public static void main(String[] args) {
+        Person p = new Person();
+        p.setName("我是马化腾");
+        p.setAge(45);
+        PersonCrossTest(p);
+        System.out.println("方法执行后的name：" + p.getName());
+    }
+}
