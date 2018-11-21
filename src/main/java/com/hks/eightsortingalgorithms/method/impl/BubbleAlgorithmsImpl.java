0b56db9e77b94ccc18d7ba8182324d95cb75f69f
heks <heks@yiche.com>
2018-11-21 12:30:09 +0800

    shit，写错了之前

--- src/main/java/com/hks/eightsortingalgorithms/method/impl/BubbleAlgorithmsImpl.java
+++ src/main/java/com/hks/eightsortingalgorithms/method/impl/BubbleAlgorithmsImpl.java
@@ -9,9 +9,9 @@ public class BubbleAlgorithmsImpl implements BubbleAlgorithms {
     @Override
     public int[] sort(int[] intArr) {
         for (int i = 0; i < intArr.length - 1; i++) {
-            for (int j = 0; j < intArr.length - 1; j++) {
+            for (int j = 0; j < intArr.length - 1 - i; j++) {
                 if (intArr[j] > intArr[j + 1]) {
-                    int temp = intArr[j + 1];
+                    int temp = intArr[j];
                     intArr[j] = intArr[j + 1];
                     intArr[j + 1] = temp;
                 }
