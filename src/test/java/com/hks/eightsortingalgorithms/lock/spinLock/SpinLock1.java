c233b97f2e8e42d74b64ff0f4d5be50ce00205de
heks <heks@yiche.com>
2019-03-26 17:00:43 +0800

    可重入锁的研究和探讨

---
+++ src/test/java/com/hks/eightsortingalgorithms/lock/spinLock/SpinLock1.java
@@ -0,0 +1,42 @@
+/*
+ * 文件名：SpinLock1.java
+ * 版权：Copyright by www.yiche.com
+ * 描述：
+ * 修改人：hekuangsheng
+ * 修改时间：2019/3/26
+ * 跟踪单号：
+ * 修改单号：
+ * 修改内容：
+ */
+package com.hks.eightsortingalgorithms.lock.spinLock;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+public class SpinLock1 {
+    private AtomicReference<Thread> owner =new AtomicReference<>();
+    private int count =0;
+    public void lock(){
+        Thread current = Thread.currentThread();
+        if(current==owner.get()) {
+            count++;
+            return ;
+        }
+
+        while(!owner.compareAndSet(null, current)){
+
+        }
+    }
+    public void unlock (){
+        Thread current = Thread.currentThread();
+        if(current==owner.get()){
+            if(count!=0){
+                count--;
+            }else{
+                owner.compareAndSet(current, null);
+            }
+
+        }
+
+    }
+}
+
